685bd060575376b9556fdb092f8fa7d75636ae84
Luke Smith <luke.smith@stu.ocsb.ca>
2024-05-28 08:55:15 -0400

    Periodic Save

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/**
+ * RST Project
+ *
+ * Author: Luke Smith
+ */
+
+'use strict';
+
+document.getElementById("space03").addEventListener("click", onSelect);
+
+function onSelect() {
+  console.log("This is a test");
+}
