61cd9476be9d2b7aea390299d6d4638a7a9f98b2
LSForSchool <159921583+LSForSchool@users.noreply.github.com>
2024-05-30 13:52:19 +0000

    Regular save

--- scripts/main.js
+++ scripts/main.js
@@ -6,44 +6,26 @@
 
 'use strict';
 
-document.getElementById("space0_0").addEventListener("click", onSelect0_0);
-document.getElementById("space0_1").addEventListener("click", onSelect0_1);
-document.getElementById("space0_2").addEventListener("click", onSelect0_2);
-document.getElementById("space0_3").addEventListener("click", onSelect0_3);
-
-document.getElementById("space1_0").addEventListener("click", onSelect1_0);
-document.getElementById("space1_1").addEventListener("click", onSelect1_1);
-document.getElementById("space1_2").addEventListener("click", onSelect1_2);
-document.getElementById("space1_3").addEventListener("click", onSelect1_3);
-
-document.getElementById("space2_0").addEventListener("click", onSelect2_0);
-document.getElementById("space2_1").addEventListener("click", onSelect2_1);
-document.getElementById("space2_2").addEventListener("click", onSelect2_2);
-document.getElementById("space2_3").addEventListener("click", onSelect2_3);
-
-document.getElementById("space3_0").addEventListener("click", onSelect3_0);
-document.getElementById("space3_1").addEventListener("click", onSelect3_1);
-document.getElementById("space3_2").addEventListener("click", onSelect3_2);
-document.getElementById("space3_3").addEventListener("click", onSelect3_3);
+document.getElementById("wordTable").addEventListener("click", onSelect);
 
 const wordGrid = [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]];
+const selectedTiles = ["empty", "empty", "empty", "empty"]
 
+function onSelect(event) {
+  let checkTileLock = false;
 
-function onSelect0_0() {
-  selectColour();
-}
-
-function selectColour() {
-  for (let index = 0; index <= 3; index++) {
-    for (let sIndex = 0; sIndex <= 3; sIndex++) {
-      let coordVariable = "space" + String(index) + "_" + String(sIndex);
-
-      if (document.getElementById(coordVariable).innerText == "Kneecap check") {
-        document.getElementById(coordVariable).innerText = "'sup?";
-      } else {
-        document.getElementById(coordVariable).innerText = "Kneecap check";
-      }
-
+  // Add Deselect Function
+  for (let checkTile = 0; checkTile <= 3; checkTile++) {
+    if (selectedTiles[checkTile] == "empty" && !checkTileLock) {
+      selectedTiles[checkTile] = event.target.id;
+      checkTileLock = true;
     }
+  }
+
+  // Colour Change
+  if (document.getElementById(event.target.id).style.backgroundColor == "bisque") {
+    document.getElementById(event.target.id).style.backgroundColor = "sandybrown";
+  } else {
+    document.getElementById(event.target.id).style.backgroundColor = "bisque";
   }
 }
