5d8d0190e7157228a41dbaacff1425b421f90aa0
LSForSchool <luke.smith@stu.ocsb.ca>
2024-06-03 10:05:34 -0400

    Selection colour is now limited to 4 at a time

--- scripts/main.js
+++ scripts/main.js
@@ -6,20 +6,25 @@
 
 'use strict';
 
+// Event Listeners
 document.getElementById("wordTable").addEventListener("click", onSelect);
 document.getElementById("clearArray").addEventListener("click", clearArray);
 
+// Global Variables
 const wordGrid = [[false, false, false, false], [false, false, false, false], [false, false, false, false], [false, false, false, false]];
 const selectedTiles = ["empty", "empty", "empty", "empty"]
 
 function onSelect(event) {
+  // Local Variables
   let checkTileLock = false;
   let deselectLock = false;
+  let colourLock = fullSelections();
 
   // Checks to see if the new selected item is already in the array of selected tiles. I don't want duplicates
   for (let checkTile = 0; checkTile <= 3; checkTile++) {
     if (selectedTiles[checkTile] == event.target.id) {
       selectedTiles[checkTile] = "empty";
+      colourLock = fullSelections();
       deselectLock = true;
     }
   }
@@ -33,13 +38,27 @@ function onSelect(event) {
   }
 
   // Colour Change
-  if (document.getElementById(event.target.id).style.backgroundColor != "sandybrown") {
-    document.getElementById(event.target.id).style.backgroundColor = "sandybrown";
-  } else {
-    document.getElementById(event.target.id).style.backgroundColor = "bisque";
+  if (!colourLock) {
+    if (document.getElementById(event.target.id).style.backgroundColor != "sandybrown") {
+      document.getElementById(event.target.id).style.backgroundColor = "sandybrown";
+    } else {
+      document.getElementById(event.target.id).style.backgroundColor = "bisque";
+    }
   }
 }
 
+// Checks the array of selections to see if it's full
+function fullSelections() {
+  for (let checkTile = 0; checkTile <= 3; checkTile++) {
+    if (selectedTiles[checkTile] == "empty") {
+      return false;
+    } else if (checkTile == 3 && selectedTiles[checkTile] != "empty") {
+      return true;
+    }
+  }
+}
+
+// Adds a button that instantly clears selections
 function clearArray() {
   for (let zamboniX = 0; zamboniX <= 3; zamboniX++) {
     selectedTiles[zamboniX] = "empty";
